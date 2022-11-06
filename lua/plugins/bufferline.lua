1787966abd68119c0d420c378e2ed17a06f3df44
Aaron Feigenbaum <adace123@users.noreply.github.com>
2022-11-06 01:16:29 -0700

    add bufferline config
    Signed-off-by: Aaron Feigenbaum <adace123@users.noreply.github.com>

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,24 @@
+local status_ok, bufferline = pcall(require, "bufferline")
+if not status_ok then
+	return
+end
+
+bufferline.setup({
+	options = {
+		offsets = {
+			{ filetype = "NvimTree", text = "", padding = 1 },
+			{ filetype = "neo-tree", text = "", padding = 1 },
+		},
+		diagnostics = "nvim_lsp",
+		diagnostics_indicator = function(_, _, diagnostics_dict, _)
+			local s = ""
+			for e, n in pairs(diagnostics_dict) do
+				local sym = e == "error" and " " or (e == "warning" and " " or " ")
+				s = s .. n .. sym
+			end
+			return s
+		end,
+		numbers = "ordinal",
+		separator_style = "slant",
+	},
+})
